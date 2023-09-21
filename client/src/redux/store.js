14a86790da38cd967cff7a67853b5aeb6a74b1ed
quang huy <emsiqh>
2023-09-21 16:35:39 +0700

    save item to local storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
 import favSlice from "./slices/favSlice";
+
+const saveToLocalStorage = (store) => (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem('currentItems', JSON.stringify(store.getState()));
+    return result;
+};
+
+const persistedState = localStorage.getItem('currentItems')
+    ? JSON.parse(localStorage.getItem('currentItems'))
+    : {};
 
 const store = configureStore({
     reducer: {
         cart: cartSlice,
         favorites: favSlice,
     },
-})
+    preloadedState: persistedState,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(saveToLocalStorage),
+});
 
 export default store;
